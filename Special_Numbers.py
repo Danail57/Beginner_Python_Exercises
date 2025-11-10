043e8fd899a3e1172dbd0efe44f2493b7991218b
Danail Donkov <danail.donkov5@gmail.com>
2025-11-10 19:58:22 +0200

    Update and rename Special_Numbers to Special_Numbers.py

--- Special_Numbers.py
+++ Special_Numbers.py
@@ -1,5 +1,5 @@
-Write a program that reads an integer and prints whether each number from 1 to that number (inclusive) is “special” or not.
-A number is considered special if the sum of its digits is 5, 7, or 11.
+# Write a program that reads an integer and prints whether each number from 1 to that number (inclusive) is “special” or not.
+# A number is considered special if the sum of its digits is 5, 7, or 11.
 
 number = int(input())
 for current_number in range(1, number + 1):
